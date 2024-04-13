1f6b0e24e032226396a19215272001509885f898
ivan-ivanovski <ivan.ivanovski@students.finki.ukim.mk>
2024-04-13 18:17:12 +0200

    Fix wrong condition in filterOddLengthStrings

--- SILab1_1.java
+++ SILab1_1.java
@@ -5,12 +5,11 @@ public class SILab1_1 {
     public static List<String> filterOddLengthStrings(List<String> list) {
         List<String> result = new ArrayList<>();
         for (String el : list) {
-            if (el.length() % 2 == 0) {
+            if (el.length() % 2 != 0) { // proverka za neparna dolzina na stringovi
                 result.add(el);
             }
         }
         return result;
     }
-
 
 }
